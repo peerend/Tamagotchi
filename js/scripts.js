69c4a237936ddd97c509abf4b42352391582b43e
Jeremy Whitaker & Dylan Peerenboom <student@epicodus.com>
2014-07-18 16:31:01 -0700

    all tests can pass, not controlled for random, need to improve tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,45 @@
-var Monster = {
-  initialize : function(creature, foodLevel, sleepLevel, activityLevel){
-    this.creature = creature,
-    this.foodLevel = foodLevel,
-    this.sleepLevel = sleepLevel,
-    this.activityLevel = activityLevel
+var Mogwai = {
+  initialize : function(creature, foodLevel, playLevel, dirtyLevel){
+    this.creature = creature;
+    this.foodLevel = foodLevel; //1 out of 4 feedings = after midnight
+    this.playLevel = playLevel; //1 out every 20 time mogwai gets play he is exposed to sunligh
+    this.dirtyLevel = dirtyLevel; //1 of 8 mogwai cleaned it gets wet
+    this.lost;
   },
-  timePasses: function(){
-    setInterval(function(){
-      this.foodLevel = this.foodLevel-1,
-      this.sleepLevel = this.sleepLevel-1,
-      this.activityLevel = this.activityLevel-1
-    },1000);
+  playLevels : function (){
+    var playRisk = Math.floor((Math.random() * 20) + 1);
+    if (playRisk === 1){
+      this.playLevel = this.playLevel - 9;
+    }
+  },
+  foodLevels : function (){
+    var foodRisk = Math.floor((Math.random() * 4) + 1);
+    if (foodRisk === 1){
+      this.foodLevel = this.foodLevel - 1;
+    }
+  },
+  dirtyLevels : function(){
+    var dirtyRisk = Math.floor((Math.random() * 8) + 1);
+    if (dirtyRisk === 1){
+      this.dirtyLevel = this.dirtyLevel-1;
+    }
+  },
+  youLost : function(){
+      if(this.playLevel <=0 || this.foodLevel <=0 || this.dirtyLevel <= 0){
+        this.lost = true;
+        //return this.youLose;
+      } else {
+        this.lost = false};
   }
 }
-
 
 //when button is clicked, s
 
   // var myMonster = Object.create(Monster);
-  // myMonster.initialize(inputtedCreature, inputtedFoodLevel, inputtedPet, inputtedSleepLevel, inputtedActivityLevel);
+  // // myMonster.initialize(inputtedCreature, inputtedFoodLevel, inputtedPet, inputtedSleepLevel, inputtedActivityLevel);
+  // timePasses: function(){
+  //   setInterval(function(){
+  //     this.foodLevel = this.foodLevel-1,
+  //     this.playLevel = this.playLevel-1,
+  //     this.dirtyLevel = this.dirtyLevel-1
+  //   },1000);
--- spec/specs.js
+++ spec/specs.js
@@ -1,34 +1,47 @@
-describe("Monster", function() {
+describe("Mogwai", function() {
   describe("initialize", function() {
     it("sets the creature type and a few other properties", function() {
-      var myMonster = Object.create(Monster);
-      myMonster.initialize("lil dragon", 10, 10, 10);
-      myMonster.creature.should.equal("lil dragon");
-      myMonster.foodLevel.should.equal(10);
-      myMonster.sleepLevel.should.equal(10);
-      myMonster.activityLevel.should.equal(10);
+      var myMogwai = Object.create(Mogwai);
+      myMogwai.initialize("Mogli", 10, 10, 10);
+      myMogwai.creature.should.equal("Mogli");
+      myMogwai.foodLevel.should.equal(10);
+      myMogwai.playLevel.should.equal(10);
+      myMogwai.dirtyLevel.should.equal(10);
     });
   });
-
-  describe("timePasses", function() {
-    it("decreases the amount of food the Tamogatchi has left by 1", function() {
-      var myMonster = Object.create(Monster);
-      myMonster.initialize("lil dragon",10,10,10);
-      myMonster.timePasses();
-      myMonster.foodLevel.should.equal(9);
+  describe("playLevel", function() {
+    it("Mogwai exposed to sunlight. Nearly or dead if under 0 life points.", function() {
+      var myMogwai = Object.create(Mogwai);
+      myMogwai.initialize("Mogli",10,10,10);
+      myMogwai.playLevels();
+      myMogwai.playLevel.should.equal(1);
     });
   });
-
-  describe("isAlive", function() {
-    it("is alive if the food level is above 0", function() {
-      var myMonster = Object.create(Monster);
-      myMonster.isAlive().should.equal(true);
+  describe("foodLevel", function() {
+    it("Mogwai eats after midnight and turns into Gremlin!!!", function() {
+      var myMogwai = Object.create(Mogwai);
+      myMogwai.initialize("Mogli",10,10,10);
+      myMogwai.foodLevels();
+      myMogwai.foodLevel.should.equal(9);
     });
-
-    it("is dead if the food level is 0", function() {
-      var myMonster = Object.create(Monster);
-      myMonster.foodLevel = 0;
-      myMonster.isAlive().should.equal(false);
+  });
+    describe("dirtyLevel", function() {
+    it("Mogwai gets wet and multiplies all over the place!", function() {
+      var myMogwai = Object.create(Mogwai);
+      myMogwai.initialize("Mogli",10,10,10);
+      myMogwai.dirtyLevels();
+      myMogwai.dirtyLevel.should.equal(9);
+    });
+  });
+  describe("youLost", function() {
+    it("you've lost if Mogwai is gremlin / dead / wet", function(){
+      var myMogwai = Object.create(Mogwai);
+      myMogwai.initialize("Mogli",1,1,1);
+      myMogwai.foodLevels();
+      myMogwai.dirtyLevels();
+      myMogwai.playLevels();
+      myMogwai.youLost();
+      myMogwai.lost.should.equal(true);
     });
   });
 });
